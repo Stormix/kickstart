5935a64761296a1be777bab750a3304b4ada1998
Anas Mazouni <madadj4@gmail.com>
2020-08-17 16:24:23 +0200

    fix: telescope not found on production
    fix: telescope not found on production
    
    
    fix: love & hate laravel production checks
    
    fix: I'm dumb, migrations should run on entrypoint
    
    build: :fire:
    
    fix: no need for migrations & seeds

--- .deploy/entrypoint.sh
+++ .deploy/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 # php artisan migrate --no-interaction --force
+# php artisan db:seed --no-interaction --force
 echo "🎬 start supervisord"
 supervisord -c $LARAVEL_PATH/.deploy/config/supervisor.conf
