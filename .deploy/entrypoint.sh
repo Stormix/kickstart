5b49fcf2e69b49bffa620063d726bfa930c34b88
Anas Mazouni <madadj4@gmail.com>
2020-08-17 22:15:01 +0200

    ci: :robot: Configuring demo CI/CD (part 1 of many)
    ci: :rocket: more CD errors pepeHands
    
    fix: wrong dockerfile was being used & tar
    
    fix: tar command
    
    fix: missing cron in the Dockerfile
    
    fix: one more package missing
    
    
    fix: one more package
    
    ci: 🚀 more CD errors pepeHands

--- .deploy/entrypoint.sh
+++ .deploy/entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-# php artisan migrate --no-interaction --force
-# php artisan db:seed --no-interaction --force
+echo "🎬 artisan commands"
+
+php artisan cache:clear
+php artisan migrate --no-interaction --force
 
 echo "🎬 start supervisord"
 supervisord -c $LARAVEL_PATH/.deploy/config/supervisor.conf
--- scripts/docker-install-yarn.sh
+++ scripts/docker-install-yarn.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
 curl -o- -L https://yarnpkg.com/install.sh | bash -s -- --version $YARN_VERSION
-
-/root/.yarn/bin/yarn global add webpack-cli
