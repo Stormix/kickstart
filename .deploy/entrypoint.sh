1299181b0ae7a53f7383e4a7a2c5eb1e7dd83ecb
Anas Mazouni <madadj4@gmail.com>
2020-08-17 16:49:17 +0200

    temp: install faker on prod

--- .deploy/entrypoint.sh
+++ .deploy/entrypoint.sh
@@ -2,5 +2,6 @@
 
 # php artisan migrate --no-interaction --force
 # php artisan db:seed --no-interaction --force
+
 echo "🎬 start supervisord"
 supervisord -c $LARAVEL_PATH/.deploy/config/supervisor.conf
